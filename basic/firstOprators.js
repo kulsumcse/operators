08aec43c2a11880f37632fde12d1c2a457fefd44
Ummey Kulsum <ummeykulsumcse@gmail.com>
2025-04-26 18:13:41 +0000

    operators

---
+++ basic/firstOprators.js
@@ -0,0 +1,70 @@
+let x = 3;
+const y = x++;
+
+
+//console.log(`x:${x}, y:${y}`);
+console.log(`x:${x},y:${y}`);
+
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+//Inequality
+console.log(1 != 1);
+// Expected output: false
+
+console.log("hello" != "hello");
+// Expected output: false
+
+console.log("1" != 1);
+// Expected output: false
+
+console.log(0 != false);
+// Expected output: false
+
+
+//Equality operator
+
+console.log(1 == 1);
+// Expected output: true
+
+console.log("hello" == "hello");
+// Expected output: true
+
+console.log("1" == 1);
+// Expected output: true
+
+console.log(0 == false);
+// Expected output: true
+
+
+//Exponentiation (**) operator
+
+
+console.log(5 ** 3);
+// Expected output: 125
+
+console.log(5 ** -2);
+// Expected output: 0.04
+
+console.log(4 **(2 ** 3));
+// Expected output: 65536
+
+console.log(3 ** 4);
+// Expected output: 81
+
+console.log(10 ** -2);
+// Expected output: 0.01
+
+console.log(2 ** (3 ** 2));
+// Expected output: 512
+
+console.log((2 ** 3) ** 2);
+// Expected output: 64
+
+
+
